612cf291483914cb6104d6f047d62c610e85e243
Lauren Livingston <laurenl@fiber.net>
2016-01-26 18:07:42 -0700

    account creation is working

--- account/views/new_user.py
+++ account/views/new_user.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django import forms
+from homepage import models as hmod
 
 def new_user(request):
     '''create a new user'''
@@ -8,44 +9,40 @@ def new_user(request):
     # create the new user form
     form = New_User_Form()
     
+    if request.method == 'POST':
+        form = New_User_Form(request.POST)
+        
+        # if the form passes my clean methods . . .
+        # also prevents SQL injection
+        if form.is_valid():
+            newUser = hmod.User()
+            newUser.first_name = form.cleaned_data['first_name']
+            newUser.last_name = form.cleaned_data['last_name']
+            newUser.username = form.cleaned_data['username']
+            newUser.set_password(form.cleaned_data['password'])
+            newUser.email = form.cleaned_data['email']
+            newUser.phone = form.cleaned_data['phone']
+            newUser.save()
+            
     # the equivalent of template_vars in DMP
     context = {
         'form' : form,
     }
-    return render(request, 'account/new_user.html', context)
+    return render(request, 'account/profile.html', context)
     
 class New_User_Form(forms.Form):
     first_name = forms.CharField(widget=forms.TextInput())
     last_name = forms.CharField(widget=forms.TextInput())
     username = forms.CharField(widget=forms.TextInput())
     password = forms.CharField(widget=forms.PasswordInput())
-    email = forms.EmailField(widget=forms.TextInput())
-    phone = forms.IntegerField(widget=forms.TextInput())
-    
-    # Not sure if we need this
-    # def __init__(self, *args, **kwargs):
-    #     self.request = kwargs.pop('request', None)
-    #     super(New_User_Form, self).__init__(*args, **kwargs)
+    email = forms.EmailField(widget=forms.EmailInput())
+    phone = forms.IntegerField(widget=forms.TextInput(), required=False)
  
     # checks to make sure that the username isn't already taken   
-    # We will probably need this eventually as well 
-    # def clean_username(self):
-    #
-    #     if self.request.urlparams[0] == 'new':
-    #         try:
-    #             smod.User.objects.get(username = self.cleaned_data['username'])
-    #             raise forms.ValidationError("That username is already taken. Please choose another.")
-    #         except smod.User.DoesNotExist:
-    #             pass
-    #         return self.cleaned_data['username']
-    #     else:
-    #         current_user = smod.User.objects.get(id = self.request.urlparams[0])
-    #         if current_user.username == self.cleaned_data['username']:
-    #             return self.cleaned_data['username']
-    #         else:
-    #             try:
-    #                 smod.User.objects.get(username = self.cleaned_data['username'])
-    #                 raise forms.ValidationError("That username is already taken. Please choose another.")
-    #             except smod.User.DoesNotExist:
-    #                 pass
-    #             return self.cleaned_data['username']
+    def clean_username(self):
+        try:
+            hmod.User.objects.get(username = self.cleaned_data['username'])
+            raise forms.ValidationError("That username is already taken. Please choose another.")
+        except hmod.User.DoesNotExist:
+            pass
+        return self.cleaned_data['username']
