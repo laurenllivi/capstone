cee9c520422d1c42c84571d92d53c256b3b8f4b7
Lauren Livingston <laurenl@fiber.net>
2016-02-11 12:54:35 -0700

    javascript photo icon

--- static/js/my_venues.js
+++ static/js/my_venues.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
     
     //hover effect for venues that need a default image
-    $(function(){
-        
-        $('.needs-photo').stop().mouseenter(function(){
-            
+    $(function(){  
+        $('.venue-pic-wrapper').stop().mouseenter(function(){
+            $(this).find('.tiny-camera-icon').css('visibility', 'visible') 
         }).stop().mouseleave(function(){
-            
-        });//mouseenter
-        
+            $(this).find('.tiny-camera-icon').css('visibility', 'hidden') 
+        });//mouseenter 
     });
     
 });
