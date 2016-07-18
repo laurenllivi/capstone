5598d1143c763bb5e225f33847af4f75c36b47b5
Lauren Livingston <laurenl@fiber.net>
2016-07-18 16:05:46 -0600

    working on adding reservation details to email and confirmation page after payment

--- payment/views/success.py
+++ payment/views/success.py
@@ -14,11 +14,16 @@ def success(request, rental_request_id):
     user = request.user
     rental_request = hmod.Rental_Request.objects.get(id=rental_request_id)
     listing = hmod.Listing.objects.get(id=rental_request.listing.id)
-    
+    listing_date = hmod.Listing_Date.objects.get(id=rental_request.listing_date.id)
+    listing_policy = hmod.Listing_Policy.objects.get(listing_id=listing.id)
+    cancellation_policy = hmod.Cancellation_Policy.objects.get(id=listing_policy.cancellation_policy_id)    
     
     context = {
        'user': user,
+       'listing': listing,
        'rental_request': rental_request,
+       'cancellation_policy': cancellation_policy,
+       'listing_date': listing_date,
     }       
 
     return render(request, 'payment/success.html', context)
